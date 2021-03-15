201dd51b0b12f4f87f291d953f3dfcfe7b29bd09
Eddy <bysky5.44.32.164@gmail.com>
2021-03-16 01:10:41 +0800

    101
    65

---
+++ pdfexport/pdfexport/ExportPDF.cs
@@ -0,0 +1,110 @@
+﻿using iTextSharp.text;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using iTextSharp.text.pdf;
+using System.IO;
+using System.Windows.Documents;
+
+namespace pdfexport
+{
+    class ExportPDF
+    {
+        public  string MachineID = "";
+        public  string ChipID = "";
+        public  string Data = "";
+        public  string T1 = "";
+        public  string T2 = "";
+        public  string AG = "";
+        public  string DG = "";
+        public  string A0 = "";
+        public  string A1 = "";
+        public  string A2 = "";
+        public  string A3 = "";
+        public  string ROI_X = "";
+        public  string ROI_W = "";
+        public  string ROI_Y = "";
+        public  string ROI_H = "";
+        public  string OP = "";
+
+        public  bool BT_Result = false;//藍芽測試 
+        public  bool MT_Result = false;//馬達測試 
+        public  bool LED_Result = false;//LED 測試
+        public  bool SP_Result = false; // 光譜測試 
+
+        public static string Note = "";
+        public static string Path = "";
+
+
+        public  void DoExport(string Path) {
+            Document document = new Document();
+            iTextSharp.text.Font title_font = new iTextSharp.text.Font(BaseFont.CreateFont(), 23);
+            //    Chunk chunk1 = new Chunk("This text is underlined",FontFactory.GetFont(FontFactory.TIMES_ROMAN, "12", Font.Underline));
+            Chunk chunk1 = new Chunk("Spectrochips", FontFactory.GetFont(FontFactory.TIMES_ROMAN, "32"));
+
+            PdfWriter.GetInstance(document, new FileStream(ChipID + ".pdf", FileMode.Create));
+
+            PdfPTable aTable = new PdfPTable(2);
+            PdfPTable bTable = new PdfPTable(4);
+            PdfPTable cTable = new PdfPTable(2);
+            PdfPTable dTable = new PdfPTable(2);
+            PdfPTable eTable = new PdfPTable(1);
+            //-A-TABLE定義------------
+            aTable.AddCell("Machine ID");
+            aTable.AddCell(MachineID);
+            aTable.AddCell("Chip ID ");
+            aTable.AddCell(ChipID);
+            aTable.AddCell("Date");
+            aTable.AddCell(Data);
+            //-B-TABLE定義------------
+            bTable.AddCell("T1/T2 ");
+            bTable.AddCell(T1+" / "+T2);
+            bTable.AddCell("AG/DG ");
+            bTable.AddCell(AG + " / " + DG);
+            //-C-TABLE定義------------
+            cTable.AddCell("A0 A1 A2 A3 ");
+            cTable.AddCell(A0+","+ A1 + "," + A2 + "," + A3);
+            cTable.AddCell("ROI ");
+            cTable.AddCell("["+ROI_X+","+ ROI_W + "," + ROI_Y + "," + ROI_H +  "]");
+            cTable.AddCell("Operator");
+            cTable.AddCell(OP);
+
+            //-D-TABLE定義------------
+            dTable.AddCell("Test Items ");
+            dTable.AddCell("Test Result");
+            dTable.AddCell("Blue Tooth Test ");
+            dTable.AddCell(BT_Result?"Pass":"NG");
+            dTable.AddCell("Motor Test ");
+            dTable.AddCell(MT_Result ? "Pass" : "NG");
+            dTable.AddCell("LED Test");
+            dTable.AddCell(LED_Result ? "Pass" : "NG");
+            dTable.AddCell("Spectral Test");
+            dTable.AddCell(SP_Result ? "Pass" : "NG");
+
+            //-E-TABLE定義------------
+            eTable.AddCell("Note: "+ Note);
+
+
+
+            //--寫入檔案---------------------
+            document.Open();
+            document.Add(new iTextSharp.text.Paragraph("Spectrochips", title_font));
+            document.Add(new iTextSharp.text.Paragraph("\n\n"));
+            document.Add(aTable);
+            document.Add(bTable);
+            document.Add(cTable);
+            document.Add(new iTextSharp.text.Paragraph("\n\n"));
+            document.Add(dTable);
+            document.Add(eTable);
+            document.Close();
+
+        }
+    }
+}
---
+++ pdfexport/pdfexport/Form1.cs
@@ -0,0 +1,99 @@
+﻿using iTextSharp.text;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using iTextSharp.text.pdf;
+using System.IO;
+using System.Windows.Documents;
+
+namespace pdfexport
+{
+
+
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Document document = new Document();
+
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Document document = new Document();
+            iTextSharp.text.Font title_font = new iTextSharp.text.Font(BaseFont.CreateFont(),23);
+        //    Chunk chunk1 = new Chunk("This text is underlined",FontFactory.GetFont(FontFactory.TIMES_ROMAN, "12", Font.Underline));
+            Chunk chunk1 = new Chunk("Spectrochips", FontFactory.GetFont(FontFactory.TIMES_ROMAN, "32"));
+
+            PdfWriter.GetInstance(document, new FileStream("Chap0101.pdf",   FileMode.Create));
+
+            PdfPTable aTable = new PdfPTable(2);
+            PdfPTable bTable = new PdfPTable(4);
+            PdfPTable cTable = new PdfPTable(2);
+            //-A-TABLE------------
+            aTable.AddCell("機台");
+            aTable.AddCell("V6-1001 ");
+            aTable.AddCell("晶片 ");
+            aTable.AddCell("20B4-116-XX  ");
+            aTable.AddCell("日期  ");
+            aTable.AddCell("2021.03.10  ");
+            //-B-TABLE------------
+            bTable.AddCell("T1/T2 ");
+            bTable.AddCell("12/12 ");
+            bTable.AddCell("AG/DG ");
+            bTable.AddCell("88/88 ");
+            //-C-TABLE------------
+            cTable.AddCell("A0 A1 A2 A3 ");
+            cTable.AddCell("1.25, 454,64 ");
+            cTable.AddCell("ROI ");
+            cTable.AddCell("[20,20,20,20]");
+            cTable.AddCell("操作者 ");
+            cTable.AddCell("邱宏swag  ");
+
+
+            document.Open();
+            document.Add(new iTextSharp.text.Paragraph("Spectrochips", title_font));
+            document.Add(new iTextSharp.text.Paragraph(chunk1));
+            document.Add(aTable);
+            document.Add(bTable);
+            document.Add(cTable);
+            document.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ExportPDF pdf = new ExportPDF();
+            
+            pdf.MachineID = "V6-1001";
+            pdf.ChipID = "20B4-116-XX";
+            pdf.Data = "2021.03.10";
+            pdf.T1 = "12";
+
+            pdf.A2 = "5";
+
+            pdf.SP_Result = true;
+
+            pdf.DoExport("path");
+
+
+
+
+        }
+    }
+}
